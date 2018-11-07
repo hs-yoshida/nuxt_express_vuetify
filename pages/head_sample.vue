ed7d655a6b6955387122d9ac7505fcd30b760fa9
hs-yoshida <hirofumi-yoshida@science.co.jp>
2018-11-07 15:30:11 +0900

    2018/11/07_15:30:10.98

--- pages/head_sample.vue
+++ pages/head_sample.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>{{ title }}</h1>
   <div>
+    <p>{{ string }}</p>
     <nuxt/>
   </div>
 </template>
@@ -9,12 +10,14 @@
 export default {
   data () {
     return {
-      title: 'Hello World!'
+      title: 'Hello World!',
+      paragraph: 'Hi there!'
     }
   },
   head () {
     return {
       title: this.title,
+      string: this.paragraph,
       meta: [
         { hid: 'description', name: 'description', content: 'My custom description' }
       ]
