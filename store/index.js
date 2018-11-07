6063296501646b86eef37d0fa1fa96e80211251d
hs-yoshida <hirofumi-yoshida@science.co.jp>
2018-11-07 17:56:25 +0900

    2018/11/07_17:56:24.62

--- store/index.js
+++ store/index.js
@@ -1,26 +1,26 @@
-// import Vuex from 'vuex'
+import Vuex from 'vuex'
 
-// const createStore = () => {
-//   return new Vuex.Store({
-//     state: () => ({
-//       counter: 0
-//     }),
-//     mutations: {
-//       increment (state) {
-//         state.counter++
-//       }
-//     }
-//   })
-// }
-
-// export default createStore
-
-export const state = () => ({
-  counter: 0
-})
-
-export const mutations = {
-  increment (state) {
-    state.counter++
-  }
+const createStore = () => {
+  return new Vuex.Store({
+    state: () => ({
+      counter: 0
+    }),
+    mutations: {
+      increment (state) {
+        state.counter++
+      }
+    }
+  })
 }
+
+export default createStore
+
+// export const state = () => ({
+//   counter: 0
+// })
+
+// export const mutations = {
+//   increment (state) {
+//     state.counter++
+//   }
+// }
