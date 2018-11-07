381e3b67f7045603a59346c89cfcb57ee3917e2c
hs-yoshida <hirofumi-yoshida@science.co.jp>
2018-11-07 17:52:45 +0900

    2018/11/07_17:52:44.87

--- store/todo.js
+++ store/todo.js
@@ -17,35 +17,35 @@ export const mutations = {
   }
 }
 
-new Vuex.Store({
-  state: () => ({
-    counter: 0
-  }),
-  mutations: {
-    increment (state) {
-      state.counter++
-    }
-  },
-  modules: {
-    namespaced: true,
-    todos: {
-      state: () => ({
-        list: []
-      }),
-      mutations: {
-        add (state, { text }) {
-          state.list.push({
-            text,
-            done: false
-          })
-        },
-        remove (state, { todo }) {
-          state.list.splice(state.list.indexOf(todo), 1)
-        },
-        toggle (state, { todo }) {
-          todo.done = !todo.done
-        }
-      }
-    }
-  }
-})
+// new Vuex.Store({
+//   state: () => ({
+//     counter: 0
+//   }),
+//   mutations: {
+//     increment (state) {
+//       state.counter++
+//     }
+//   },
+//   modules: {
+//     namespaced: true,
+//     todos: {
+//       state: () => ({
+//         list: []
+//       }),
+//       mutations: {
+//         add (state, { text }) {
+//           state.list.push({
+//             text,
+//             done: false
+//           })
+//         },
+//         remove (state, { todo }) {
+//           state.list.splice(state.list.indexOf(todo), 1)
+//         },
+//         toggle (state, { todo }) {
+//           todo.done = !todo.done
+//         }
+//       }
+//     }
+//   }
+// })
