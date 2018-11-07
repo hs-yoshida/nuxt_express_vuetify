8f2084ee5e22b34a5f7af10943d095af82681ebb
hs-yoshida <hirofumi-yoshida@science.co.jp>
2018-11-07 14:09:25 +0900

    2018/11/07_11:18:15.22

---
+++ pages/head_sample.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Hello World!'
+    }
+  },
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'description', name: 'description', content: 'My custom description' }
+      ]
+    }
+  }
+}
+</script>
