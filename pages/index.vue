569ead1d2ac810a76afa1dda85caff293b174234
hs-yoshida <hirofumi-yoshida@science.co.jp>
2018-11-08 17:37:55 +0900

    2018/11/08_17:37:55.43

--- pages/index.vue
+++ pages/index.vue
@@ -1,76 +1,130 @@
 <template>
-  <v-layout justify-center>
-      <v-parallax
-      dark
-      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-    >
-      <v-layout
-        align-center
-        column
-        justify-center
-      >
+  <v-app>
+    <v-parallax dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+      <v-layout align-center column justify-center>
         <h1 class="display-2 font-weight-thin mb-3">humour semantics</h1>
         <h4 class="subheading">In nulla perferendis.</h4>
       </v-layout>
     </v-parallax>
-    <v-flex xs12 sm6>
-      <!-- <v-toolbar color="indigo" dark>
-        <v-toolbar-side-icon></v-toolbar-side-icon>
-        <v-toolbar-title>Discover</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>search</v-icon>
-        </v-btn>
-      </v-toolbar> -->
+    <v-container>
+      <v-layout>
+        <v-flex xs12 sm6>
+          <v-container>
+            <v-card>
+              <v-img class="white--text" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+                <v-container fill-height fluid>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                      <span class="headline">Top 10 Australian beaches</span>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-img>
+              <v-card-title>
+                <div>
+                  <span class="grey--text">Number 10</span><br>
+                  <span>Whitehaven Beach</span><br>
+                  <span>Whitsunday Island, Whitsunday Islands</span>
+                </div>
+              </v-card-title>
+              <v-card-actions>
+                <v-btn flat color="orange">Share</v-btn>
+                <v-btn flat color="orange">Explore</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-container>
+        </v-flex>
 
+        <v-flex xs12 sm6>
+          <v-container>
+            <v-card>
+              <v-img class="white--text" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+                <v-container fill-height fluid>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                      <span class="headline">Top 10 Australian beaches</span>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-img>
+              <v-card-title>
+                <div>
+                  <span class="grey--text">Number 10</span><br>
+                  <span>Whitehaven Beach</span><br>
+                  <span>Whitsunday Island, Whitsunday Islands</span>
+                </div>
+              </v-card-title>
+              <v-card-actions>
+                <v-btn flat color="orange">Share</v-btn>
+                <v-btn flat color="orange">Explore</v-btn>
+              </v-card-actions>
+            </v-card>
+          <v-container>
+        </v-flex>
 
-      <v-card>
-        <v-container
-          fluid
-          grid-list-md
-        >
-          <v-layout row wrap>
-            <v-flex
-              v-for="card in cards"
-              v-bind="{ [`xs${card.flex}`]: true }"
-              :key="card.title"
-            >
-              <v-card>
-                <v-img
-                  :src="card.src"
-                  height="200px"
-                >
-                  <v-container
-                    fill-height
-                    fluid
-                    pa-2
-                  >
-                    <v-layout fill-height>
-                      <v-flex xs12 align-end flexbox>
-                        <span class="headline white--text" v-text="card.title"></span>
-                      </v-flex>
-                    </v-layout>
-                  </v-container>
-                </v-img>
+        <v-flex xs12 sm6>
+          <v-container>
+            <v-card>
+              <v-img class="white--text" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+                <v-container fill-height fluid>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                      <span class="headline">Top 10 Australian beaches</span>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-img>
+              <v-card-title>
+                <div>
+                  <span class="grey--text">Number 10</span><br>
+                  <span>Whitehaven Beach</span><br>
+                  <span>Whitsunday Island, Whitsunday Islands</span>
+                </div>
+              </v-card-title>
+              <v-card-actions>
+                <v-btn flat color="orange">Share</v-btn>
+                <v-btn flat color="orange">Explore</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-container>
+        </v-flex>
 
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn icon>
-                    <v-icon>favorite</v-icon>
-                  </v-btn>
-                  <v-btn icon>
-                    <v-icon>bookmark</v-icon>
-                  </v-btn>
-                  <v-btn icon>
-                    <v-icon>share</v-icon>
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card>
-    </v-flex>
-  </v-layout>
+      </v-layout>
+    </v-container>
+  </v-app>
 </template>
 
+
+<script>
+  export default {
+    data: () => ({
+      cards: [{
+          title: 'Pre-fab homes',
+          src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
+          flex: 12
+        },
+        {
+          title: 'Favorite road trips',
+          src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+          flex: 6
+        },
+        {
+          title: 'Best airlines',
+          src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
+          flex: 6
+        }
+      ]
+    })
+  }
+
+</script>
+<script>
+  export default {
+    data() {
+      return {
+        card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.'
+      }
+    }
+  }
+
+</script>
