d6b554c5e6b8774ca6f98e670b1a6edb7f99465e
hs-yoshida <hirofumi-yoshida@science.co.jp>
2018-11-08 15:07:12 +0900

    2018/11/08_15:07:12.33

--- layouts/default.vue
+++ layouts/default.vue
@@ -56,6 +56,13 @@
         <v-icon>menu</v-icon>
       </v-btn>
     </v-toolbar>
+    <v-carousel>
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
     <v-content>
       <v-container>
         <nuxt />
@@ -111,3 +118,24 @@
     }
   }
 </script>
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+          }
+        ]
+      }
+    }
+  }
